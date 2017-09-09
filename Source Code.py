6b6a81e33577ba2e2556ae272a046bd801cb1c51
Tangudu Avinash <avinashtangudu@gmail.com>
2017-09-08 22:53:43 -0500

    Update Source Code.py

--- Source Code.py
+++ Source Code.py
@@ -1,12 +1,17 @@
+# I have imported a regex library into Python#
+# https://en.wikipedia.org/wiki/Regular_expression #
 
 import re
 
+# Calculator Title and Instruction for Quit or Stop #
 print("Simplest Calculator")
 print("To 'Exit' enter Quit\n")
 
+# Assigning a Variable #
 first = 0
 second = True
 
+# Used global options here#
 def MathCalci():
     global second
     global first
@@ -20,7 +25,8 @@ def MathCalci():
         print("Bye bye Pal")
         second = False
     else:
-        equation = re.sub('[a-zA-Z:,.()" "]','',equation)
+        equation = re.sub('[a-zA-Z:,.()" "]','',equation) # Used the (re.sub) class from re library to stop printing the ...#
+                                                           #.. letters and special characters inside a calculator #
 
         if first == 0:
             first = eval(equation)
